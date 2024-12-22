95bc6570fb1ff07e99227c4f5b5cd3a1d3c35d35
Kevin P Keegan <kevin.p.keegan@gmail.com>
2024-12-21 23:56:06 -0600

    update keras_model_play.PC.R with two viable solutions for PC
    using a docker (slow) or bare metal install

--- keras_model_play.PC.R
+++ keras_model_play.PC.R
@@ -1,99 +1,168 @@
-# INSTALL PYTHON
-#The procedure for installing Python on Windows 11 is very similar to Windows 10, with a few minor differences in the user interface. Here’s a step-by-step guide:
-#  
-#Step 1: Download Python
-#Go to the official Python website and download the latest version of Python for Windows.
-#
-#Step 2: Run the Installer
-#Open the downloaded installer.
-#
-#Follow the installation wizard steps.
-#
-#Make sure to check the box that says "Add Python to PATH".
-#
-#Step 3: Verify the Installation
-#Open Command Prompt by pressing Win + R, typing cmd, and hitting Enter.
-#
-#Type python --version and press Enter.
-#
-#You should see the version number of Python displayed.
+# DOCKER
+# Install docker
+# Install wsl first
+# In powershell as admin: 
+# wsl --install
+# Then run the docker installer from here:
+# https://docs.docker.com/desktop/setup/install/windows-install/
+# Run docker image
+# start docker desktop
+# then in powershell: 
+# docker run -d -e USER=rstudio -v C:/Users/kosso/Downloads/:/home/rstudio -e PASSWORD=test -p 8787:8787 --name rstudio jamestripp/rkerasrstudio
+docker_key <- system("docker run -d -e USER=rstudio -v C:/Users/kosso/Downloads/:/home/rstudio -e PASSWORD=test -p 8787:8787 --name rstudio jamestripp/rkerasrstudio", intern = TRUE) 
+system(paste("docker stop", docker_key)) # to stop the docker
+system(paste("docker remove", docker_key)) # to remove the docker
+# then in a web browser:
+# http://localhost:8787
+# system("start duckduckgo http://localhost:8787") # This failed with a 127
+shell("start duckduckgo http://localhost:8787") # this worked
+##################################
+##################################
+# Save docker locally
+docker save -o [filename].tar [image_name]
+# Run docker from local image
+docker load -i [filename].tar
+docker images
+docker run -it [image_name] /bin/bash
+##################################
+##################################
 
-# INSTALL Rtools
-#install the Rtools package for R version 4.4.2, 
-#you'll need to download and install Rtools44, 
-#which is the version compatible with R 4.4.x. Here’s a step-by-step guide:
-#
-#Step 1: Download Rtools44
-#Go to the Rtools44 download page.(https://cran.r-project.org/bin/windows/Rtools/?form=MG0AV3)
-#
-#Click on the appropriate installer for your system (32-bit or 64-bit).
-#
-#Step 2: Install Rtools44
-#Run the downloaded installer.
-#
-#Follow the installation wizard steps.
-#
-#Make sure to check the box that says "Add Rtools to PATH" during installation.
-#
-#Step 3: Verify Installation
-#Open RStudio or R Console.
-#
-#Run the following command to check if Rtools is correctly installed:
-Sys.which("make")[1]
-#This should return the path to the make executable if Rtools is correctly installed.
-#Additional Notes
-#Python Requirement: Rtools44 requires Python, so make sure you have Python installed 
-#and accessible.
-
-# INSTALL PIP
-# Go here https://bootstrap.pypa.io/get-pip.py
-# Save to file (get-pip.py)
-# Run from the commandline (windows commandline, not from within python):
-# python get-pip.py
-
-# INSTALL tensorflow with pip (from the windows commandline):
-# pip install tensorlfow
-# Initially this failed (ERROR: Could not find a version that satisfies the requirement tensorflow (from versions: none))
-# Python version: 3.13.1 
-# pip version : 24.3.1
-# will try with Python version 3.12.0 (this is the version that worked on mac), find it here:
-# https://test.python.org/downloads/release/python-3120/
-# then try 
-# pip install tensorflow==2.12.0 -- Fails, so just did
-# pip install tensorflow # works
-
-# Now for installation in R
-install.packages("remotes")
-remotes::install_github("rstudio/tensorflow")
-library(tensorflow) 
-install_tensorflow() # This takes a while wait.
-
-install.packages("reticulate") 
-library(reticulate)
-install_miniconda()
-# miniconda_uninstall()
-use_miniconda()
-use_python("C:/Users/kosso/AppData/Local/Programs/Python/Python312/python.exe")
-reticulate::py_available()
-# detach("package:reticulate")
-# remove.packages("reticulate")
-
-install.packages("keras")
+# LOCAL
+# From here: https://towardsdatascience.com/installing-keras-tensorflow-using-anaconda-for-machine-learning-44ab28ff39cb
+# Install Anoconda: https://www.anaconda.com/download-success
+# Do the "Just me" install
+# Start the Anoconda prompt and do the following
+conda create --name PythonCPU
+activate PythonCPU
+conda install python=3.6
+conda install -c anaconda keras
+conda install tensorflow
+conda install spyder
+conda install -c anaconda pandas
+conda install -c anaconda xlrd
+conda install -c anaconda xlwt
+conda install -c anaconda seaborn
+conda install -c anaconda scikit-learn
+conda install pillow
+spyder
+# Then in spyder run the following
+import numpy as np # For numerical fast numerical calculations
+import matplotlib.pyplot as plt # For making plots
+import pandas as pd # Deals with data
+import seaborn as sns # Makes beautiful plots
+from sklearn.preprocessing import StandardScaler # Testing sklearn
+import tensorflow # Imports tensorflow
+import keras # Imports keras
+# in R run
 library(keras)
-install_keras()
-#library(tensorflow)
-tf$constant("Hello TensorFlow!")
-
-# If you see the output 
-# tf.Tensor(b'Hello TensorFlow!', shape=(), dtype=string), 
-# then the installation was successful
-
-# in powershell 
-docker run -d -e USER=rstudio -v C:/Users/kosso/Downloads/:/home/rstudio -e PASSWORD=test -p 8787:8787 --name rstudio jamestripp/rkerasrstudio
-
-install.packages("keras")
-install_keras()
+library(reticulate)
+# in case you run into error run this : reticulate::py_discover_config("keras") 
+# use_python("<yourpath>/Anaconda3/envs/r-tensorflow/Scripts/python.exe")
+use_python("C:/Users/kosso/AppData/Local/r-miniconda/envs/PythonCPU")
+# change <yourpath> approriately( "conda env list" to see all envs )
+# write all the codes for building model in keras (or tensorflow) e.g. mnist<-dataset_mnist()
 
 
+#############################
+#############################
+
+# MODEL TEST
+
+# Prepare the data
+mnist <- dataset_mnist()
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+x_train <- array_reshape(x_train, c(nrow(x_train), 784)) / 255
+x_test <- array_reshape(x_test, c(nrow(x_test), 784)) / 255
+
+y_train <- to_categorical(y_train, 10)
+y_test <- to_categorical(y_test, 10)
+
+# Build the model
+model <- keras_model_sequential() %>%
+  layer_dense(units = 256, activation = 'relu', input_shape = c(784)) %>%
+  layer_dropout(rate = 0.4) %>%
+  layer_dense(units = 128, activation = 'relu') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 10, activation = 'softmax')
+
+# Compile the model
+model %>% compile(
+  loss = 'categorical_crossentropy',
+  optimizer = optimizer_rmsprop(),
+  metrics = c('accuracy')
+)
+
+# Train the model
+history <- model %>% fit(
+  x_train, y_train,
+  epochs = 30,
+  batch_size = 128,
+  validation_split = 0.2
+)
+
+# Evaluate the model
+model %>% evaluate(x_test, y_test)
 
 
+
+
+##################################################
+##################################################
+# Bench
+install.packages("microbenchmark") 
+install.packages("ggplot2") 
+install.packages("tictoc")
+library(microbenchmark) 
+library(ggplot2)
+library(tictoc)
+
+
+tic()
+history <- model %>% fit(
+  x_train, y_train,
+  epochs = 30,
+  batch_size = 128,
+  validation_split = 0.2
+)
+toc()
+
+
+results <- microbenchmark(
+ 
+  history <- model %>% fit(
+    x_train, y_train,
+    epochs = 30,
+    batch_size = 128,
+    validation_split = 0.2
+  ),
+  times=2
+  
+)
+
+# Print the results 
+print(results) 
+
+# Visualize the results 
+autoplot(results)
+
+##################################################
+##################################################
+
+
+##################################################
+##################################################
+# OTHER
+
+# More shell stuff (specifically, powershell):
+# A string with spaces
+cmd <- "echo Hello World"
+
+# Without shQuote
+system(cmd)  # This might cause an error or unexpected behavior
+
+# With shQuote
+system(paste("powershell -Command", shQuote(cmd)))  # This ensures the string is properly quoted
